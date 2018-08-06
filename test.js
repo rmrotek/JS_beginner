1d7a8ecd497c2ebc3fe5734f126cce0e66e218e5
remigiusz mrotek <remek87@gmail.com>
2018-08-06 11:50:26 +0200

    vid18 break and continue

--- test.js
+++ test.js
@@ -2,9 +2,18 @@ var links = document.getElementsByTagName("a");
 
 
 
-for (i = 0; i < links.length; i++) {
-    console.log("this is link number" + i);
+for (i = 0; i < 10; i++) {
+
+    if (i===5 || i===3) {
+        continue;
+        
+    }
+    console.log(i);
+
+    if (i===7) {
+        break;
+    }
     
 }
 
-document.write("all links now looped");
+console.log("loop broken at " +i)
