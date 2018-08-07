42a1c6fb57c107fe968d29f4f1632ce900263d2c
remigiusz mrotek <remek87@gmail.com>
2018-08-07 13:28:09 +0200

    vid41 onload

--- test.js
+++ test.js
@@ -1,16 +1,25 @@
-var content = document.getElementById("content");
-var button = document.getElementById("show-more");
+function setUpEvents(){
+    var content = document.getElementById("content");
+    var button = document.getElementById("show-more");
+    
+    button.onclick = function(){
+    
+        if(content.className == "open"){
+            //shrink the box
+            content.className = "";
+            button.innerHTML = "show more";
+    
+        } else {
+            //expand the box
+            content.className = "open";
+            button.innerHTML = "show less";
+        }
+    };
+}
 
-button.onclick = function(){
 
-    if(content.className == "open"){
-        //shrink the box
-        content.className = "";
-        button.innerHTML = "show more";
 
-    } else {
-        //expand the box
-        content.className = "open";
-        button.innerHTML = "show less";
-    }
+window.onload = function(){
+    setUpEvents();
+
 }
