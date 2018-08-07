a8d0757abe23bf11d392e736c030ec16d75c12d0
remigiusz mrotek <remek87@gmail.com>
2018-08-07 11:01:06 +0200

    vid30 constructor

--- test.js
+++ test.js
@@ -1,17 +1,21 @@
 
 
-var myCar2 = {
-    maxSpeed: 70, 
-    driver: "Ninja", 
-    drive: function(speed, time){ console.log(speed*time);
-    },
-    logDriver: function () {
+var Car = function(maxSpeed, driver){
+    this.maxSpeed = maxSpeed;
+    this.driver = driver;
+    this.drive = function(speed, time){ 
+        console.log(speed*time);
+    };
+    this.logDriver = function () {
         console.log("driver name is "+ this.driver);
         
-    }
+    };
 };
 
-console.log(myCar2.maxSpeed);
-myCar2.drive(50, 3);
+var myCar = new Car(70, "driver 1");
+var myCar2 = new Car(40, "driver 2");
+var myCar3 = new Car(10, "driver 3");
+var myCar4 = new Car(90, "driver 4");
 
-myCar2.logDriver();
+myCar.drive(30,5);
+myCar3.logDriver();
