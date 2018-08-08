d3f4c19fd75509b0ad995983a429f72b55fb36a4
remigiusz mrotek <remek87@gmail.com>
2018-08-08 10:01:02 +0200

    vid44 form validation

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 var myForm = document.forms.myForm;
+var message = document.getElementById("message");
+
 
 myForm.name.onfocus = function(){
     myForm.name.style.border = "4px solid pink"; 
@@ -7,4 +9,16 @@ myForm.name.onfocus = function(){
     
 myForm.name.onblur = function(){
     myForm.name.style.border = "none";
+};
+
+
+myForm.onsubmit = function(){
+
+    if (myForm.name.value == "") {
+        message.innerHTML = "please enter a name";
+        return false;
+    } else {
+        message.innerHTML = "";
+        return true;
+    }
 };
