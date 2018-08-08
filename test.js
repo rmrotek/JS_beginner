f7fe286d587c1747a4ad81117eed3fc441e9ad54
remigiusz mrotek <remek87@gmail.com>
2018-08-08 10:14:35 +0200

    vid45 jquery

--- test.js
+++ test.js
@@ -1,24 +1,2 @@
-var myForm = document.forms.myForm;
-var message = document.getElementById("message");
-
-
-myForm.name.onfocus = function(){
-    myForm.name.style.border = "4px solid pink"; 
-};
-    
-    
-myForm.name.onblur = function(){
-    myForm.name.style.border = "none";
-};
-
-
-myForm.onsubmit = function(){
-
-    if (myForm.name.value == "") {
-        message.innerHTML = "please enter a name";
-        return false;
-    } else {
-        message.innerHTML = "";
-        return true;
-    }
-};
+var myPara = document.getElementById("content").getElementsByTagName("p")[5];
+var myPara2 = $("#content p:last-child");
